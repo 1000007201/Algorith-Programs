b818415679540f1339a06af9f147f6ebbb9b83b8
1000007201 <nishant.sharma5504@gmail.com>
2022-02-25 20:08:27 +0530

    [Nishant] Add:Insertion Sort Done

---
+++ Algorithm_Programs/Insertion_Sort.py
@@ -0,0 +1,19 @@
+def insertion_sort(arr):
+
+    for i in range(1, len(arr)):
+        val = arr[i]
+
+        j = i-1
+        while j >= 0 and val < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = val
+
+    return arr
+
+
+if __name__ == "__main__":
+    sample_string = "nishantsharma"
+    arr = [i for i in sample_string]
+    print(f"Unsorted List:\n{arr}")
+    print(f"Sorted List:\n{insertion_sort(arr)}")
